c0c1ef989c09dded19bc284ca2cce33b196953c7
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-07 12:19:33 +0100

    Etape 4 : Correspondance Alphabet Morse -> Alphabet Latin + espaces

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Etape 1 : Retourner un tableau de caractères
-let stringTest = "HELLO WORLD";
+let stringLatinTest = "Hello, World";
 
 getLatinCharacterList = (text) => {
   return text.split("");
 };
 
-getLatinCharacterList(stringTest);
-console.log(getLatinCharacterList(stringTest));
+getLatinCharacterList(stringLatinTest);
+console.log(getLatinCharacterList(stringLatinTest));
 
 // Etape 2 : Correspondance Alphabet Latin -> Alphabet Morse
 const latinToMorse = {
@@ -36,27 +36,86 @@ const latinToMorse = {
   X: "-..-",
   Y: "-.--",
   Z: "--..",
+  " ": "/",
 };
 
-let charactersTest = "A";
-translateLatinCharacter = (characters) => {
-  return latinToMorse[characters];
+let charactersLatinTest = "A";
+translateLatinCharacter = (charactersLatin) => {
+  return latinToMorse[charactersLatin];
 };
 
-translateLatinCharacter(charactersTest);
-console.log(translateLatinCharacter(charactersTest));
+translateLatinCharacter(charactersLatinTest);
+console.log(translateLatinCharacter(charactersLatinTest));
 
 // Etape 3 : Correspondance Alphabet Latin -> Alphabet Morse pour une phrase entière
+stringLatinTest = "HELLO WORLD";
 encode = (text) => {
   //Appeler le fonction getLatinCharacterList
   getLatinCharacterList(text);
   console.log(getLatinCharacterList(text));
   //Boucler sur chaque caractère du tableau
   for (let i = 0; i < getLatinCharacterList(text).length; i++) {
-    let char = getLatinCharacterList(text)[i];
-    translateLatinCharacter(char);
-    console.log(translateLatinCharacter(char));
+    let charLatin = getLatinCharacterList(text)[i];
+    translateLatinCharacter(charLatin);
+    console.log(translateLatinCharacter(charLatin));
   }
 };
 
-encode(stringTest);
+encode(stringLatinTest);
+
+// Etape 4 : Correspondance Alphabet Morse -> Alphabet Latin + espace entre les mots
+const morseToLatin = {
+  "-": "T",
+  "--": "M",
+  "---": "O",
+  "--.": "G",
+  "--.-": "Q",
+  "--..": "Z",
+  "-.": "N",
+  "-.-": "K",
+  "-.--": "Y",
+  "-.-.": "C",
+  "-..": "D",
+  "-..-": "X",
+  "-...": "B",
+  ".": "E",
+  ".-": "A",
+  ".--": "W",
+  ".---": "J",
+  ".--.": "P",
+  ".-.": "R",
+  ".-..": "L",
+  "..": "I",
+  "..-": "U",
+  "..-.": "F",
+  "...": "S",
+  "...-": "V",
+  "....": "H",
+  "/": " ",
+};
+
+let morseTextTest = ".... . .-.. .-.. --- / .-- --- .-. .-.. -..";
+
+// Retourne un tableau de caractères Morse
+getMorseCharacterList = (morseText) => {
+  return morseText.split(" ");
+};
+
+// Traduis un caractère Morse en caractère Latin
+translateMorseCharacter = (morseCharacter) => {
+  return morseToLatin[morseCharacter];
+};
+
+decode = (text) => {
+  //Appeler le fonction getMorseCharacterList
+  getMorseCharacterList(text);
+  console.log(getMorseCharacterList(text));
+  //Boucler sur chaque caractère du tableau
+  for (let i = 0; i < getMorseCharacterList(text).length; i++) {
+    let charMorse = getMorseCharacterList(text)[i];
+    translateMorseCharacter(charMorse);
+    console.log(translateMorseCharacter(charMorse));
+  }
+};
+
+decode(morseTextTest);
