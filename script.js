5071a3b6dbc2153eb723d61170dab590f06f8187
Laurie Lespinasse <lespinasse.laurie5@gmail.com>
2025-11-07 11:36:16 +0100

    Etape 2 : Corresponndance Alphabet Latin -> Alphabet Morse

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-string = "Hello, World";
+// Etape 1 : Retourner un tableau de caractères
+let stringTest = "HELLO WORLD";
 
 getLatinCharacterList = (text) => {
   return text.split("");
 };
 
-getLatinCharacterList(string);
-console.log(getLatinCharacterList(string));
+getLatinCharacterList(stringTest);
+console.log(getLatinCharacterList(stringTest));
+
+// Etape 2 : Correspondance Alphabet Latin -> Alphabet Morse
+const latinToMorse = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+let charactersTest = "A";
+translateLatinCharacter = (characters) => {
+  return latinToMorse[characters];
+};
+
+translateLatinCharacter(charactersTest);
+console.log(translateLatinCharacter(charactersTest));
